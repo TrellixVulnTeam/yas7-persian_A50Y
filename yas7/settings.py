4f52474f1a49622e079462e1ccd89834c6ae5afd
imanarbabi <iarbabi2020@gmail.com>
2021-01-29 19:14:14 +0330

    update for first deploy setting.py

--- yas7/settings.py
+++ yas7/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=3051/9ct2xuo%sc6+yal1f^+ces4uja-+g+wrb8h9=_^w$s92p'
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+# SECRET_KEY = '=3051/9ct2xuo%sc6+yal1f^+ces4uja-+g+wrb8h9=_^w$s92p'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS =['yasacademy.com']
+ALLOWED_HOSTS =['yasacademy.com']
 
 
 # Application definition
